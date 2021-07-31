e2e3d9113818cc4b43fcfba538d9d3ca47499f60
Lorilla Family <edwardlorilla1986@gmail.com>
2021-08-01 00:17:03 +0800

    first commit

---
+++ app/src/main/java/com/anushka/androidtutz/bindingdemo2/Student.java
@@ -0,0 +1,34 @@
+package com.anushka.androidtutz.bindingdemo2;
+
+public class Student {
+
+    private String studentName;
+    private String studentEmail;
+
+
+    public Student(String studentName, String studentEmail) {
+        this.studentName = studentName;
+        this.studentEmail = studentEmail;
+    }
+
+    public Student() {
+    }
+
+
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+}
